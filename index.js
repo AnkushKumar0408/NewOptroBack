649029c145aded3ee27940310ccb9dd12215b76d
AnkushPal0408 <90330974+AnkushPal0408@users.noreply.github.com>
2025-06-24 13:20:21 +0530

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://new-optro.vercel.app/", // your frontend URL
+    origin: "https://new-optro.vercel.app", // your frontend URL
     methods: ["GET", "POST"],
     credentials: true,
   })
