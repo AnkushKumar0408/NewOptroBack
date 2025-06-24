dc811e13e94a6be6a3b39715d8017e9bc11007e0
AnkushPal0408 <90330974+AnkushPal0408@users.noreply.github.com>
2025-06-24 12:50:49 +0530

    update

--- index.js
+++ index.js
@@ -1,25 +1,64 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("./model/user");
+const User = require("./model/user");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "https://new-optro.vercel.app/", // your frontend URL
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.get("/", (req, res) => {
   res.send("This is backend for Optronix");
 });
 
 app.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
-  let user = await mongoose.create({
-    name,
+  const {
+    fullName,
     email,
+    phone,
+    gender,
+    dob,
+    address,
     password,
+    confirmPassword,
+    latitude,
+    longitude,
+    deviceInfo,
+  } = req.body;
+  let user = await User.create({
+    fullName,
+    email,
+    phone,
+    gender,
+    dob,
+    address,
+    password,
+    confirmPassword,
+    latitude,
+    longitude,
+    deviceInfo,
   });
+  // console.log(user);
+});
+
+app.get("/customer/:phone", async (req, res) => {
+  try {
+    const customer = await mongoose.findOne({ phone: req.params.phone });
+    if (!customer) return res.status(404).send("Not found");
+    res.json(customer);
+  } catch (err) {
+    res.status(400).send("Error");
+  }
 });
 
 app.listen(process.env.PORT, () => {
   console.log("Server Running");
 });
+
+module.exports = app;
--- model/user.js
+++ model/user.js
@@ -1,12 +1,24 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI).then(console.log("Database Connected"));
+// mongoose.connect(process.env.MONGO_URI).then(console.log("Database Connected"));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const userSchema = mongoose.Schema({
-  name: String,
+  fullName: String,
   email: String,
+  phone: String,
+  gender: String,
+  dob: String,
+  address: String,
   password: String,
+  latitude: String,
+  longitude: String,
+  deviceInfo: String,
 });
 
 module.exports = mongoose.model("user", userSchema);
